b8b21781dcd3b9985892a49b45f1350b4bc999e5
Piotr <92692836+ptr96@users.noreply.github.com>
2023-06-06 17:58:01 +0200

    Update main.py
    add simple check hybridization func.

--- main.py
+++ main.py
@@ -17,11 +17,34 @@ def generate_subsequences(sequence, k):
         subsequences.append(subsequence)
     return subsequences
 
+
+def check_hybridization(seq1, seq2):
+    if len(seq1) != len(seq2):
+        return False
+
+    complement = {'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C'}
+
+    for i in range(len(seq1)):
+        if complement[seq1[i]] != seq2[i]:
+            return False
+
+    return True
+
+
 dna_sequence = generate_dna_sequence(700)
 subsequences = generate_subsequences(dna_sequence, 7)
 
-print(dna_sequence)
-print(subsequences)
+# print(dna_sequence)
+# print(subsequences)
+
+
+sequence1 = subsequences[0]
+sequence2 = subsequences[1]
+
+if check_hybridization(sequence1, sequence2):
+    print("Sekwencje hybrydyzują ze sobą.")
+else:
+    print("Sekwencje nie hybrydyzują ze sobą.")
 
 # for subsequence in subsequences:
 #     print(subsequence)

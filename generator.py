f203dbd2b7814f8d1fcb12f5b1cd8922f377c3ed
Piotr <92692836+ptr96@users.noreply.github.com>
2023-06-13 22:13:48 +0200

    Add files via upload

---
+++ generator.py
@@ -0,0 +1,56 @@
+import os
+import random
+
+from constatnts import K
+
+# stworzenie nitki DNA
+def generate_dna_sequence(length):
+    nucleotides = ['A', 'T', 'C', 'G']
+    sequence = ''
+
+    for _ in range(length):
+        nucleotide = random.choice(nucleotides)
+        sequence += nucleotide
+
+    return sequence
+
+# tworzymy zbiór oligonukleotydów
+def generate_subsequences(sequence, k):
+    subsequences = []
+    for i in range(len(sequence) - k + 1):
+        subsequence = sequence[i:i + k]
+        subsequences.append(subsequence)
+    return subsequences
+
+def create_errors(subsequences, negative, positive):
+    sequence_with_errors = subsequences
+    if negative > 0:
+        sequence_with_errors = subsequences[0:len(subsequences) - negative]
+    if positive > 0:
+        sequence_with_errors = sequence_with_errors + [generate_dna_sequence(K) for _ in range(positive)]
+    return sequence_with_errors
+
+
+def save_sequence_to_file(sequence, folder, filename):
+    folder_path = os.path.join(os.getcwd(), folder)
+
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+
+    file_path = os.path.join(folder_path, filename)
+
+    with open(file_path, 'w') as file:
+        file.write(sequence)
+
+
+def save_subsequences_to_file(subsequences, folder, filename):
+    folder_path = os.path.join(os.getcwd(), folder)
+
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+
+    file_path = os.path.join(folder_path, filename)
+
+    with open(file_path, 'w') as file:
+        for subsequence in subsequences:
+            file.write(subsequence + '\n')
